018976d96f9d0736ac6652d66abec209e1b7750d
steedos <zhuangjianguo@hotlong.com>
2016-05-12 09:46:31 +0800

    fix bbq

--- packages/steedos-connect-bqq/company_token.js
+++ packages/steedos-connect-bqq/company_token.js
@@ -9,7 +9,7 @@ JsonRoutes.add("get", "/api/bqq/companyToken", function (req, res, next) {
         params: {
           code: req.query.code,
           app_id: config.clientId,
-          redirect_uri: "http://bi.steedos.com:3000/api/bqq/companyToken",
+          redirect_uri: "https://cn.steedos.com/workflow/api/bqq/companyToken",
           app_secret: OAuth.openSecret(config.secret),
           grant_type: 'authorization_code'
         }
