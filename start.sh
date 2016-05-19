cc9a3e37c1b65a2887f2726df9df3eac6955f480
steedos <zhuangjianguo@hotlong.com>
2016-05-19 10:09:16 +0800

    fix start

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ export DB_SERVER=192.168.0.23
 export MONGO_URL="mongodb://$DB_SERVER/steedos"
 export MONGO_OPLOG_URL="mongodb://$DB_SERVER/local"
 export MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-export ROOT_URL=http://192.168.0.155:3000/workflow/
+export ROOT_URL=http://127.0.0.1:3000/workflow/
 meteor run --settings settings.json
